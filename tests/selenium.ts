dba9df1d3d39235ab9397f726affef7967ed348e
Oleg Khokhlov <oleg.khokhlov@oracle.com>
2018-11-05 12:18:39 -0800

    real test

--- tests/selenium.ts
+++ tests/selenium.ts
@@ -1,1 +1,25 @@
-#
+import { expect, assert } from 'chai';
+import { Builder, By, until, ThenableWebDriver, WebDriver } from 'selenium-webdriver';
+
+describe("All Tests", function () {
+
+    let driver: ThenableWebDriver;
+  
+    describe('Test Chrome', function () {
+  
+      beforeEach(async function () {
+        driver = new Builder().withCapabilities({ 'browserName': 'chrome' }).build();
+      });
+  
+      it('test #1', async function () {
+        await driver.get('http://www.google.com');
+        
+      });
+
+      afterEach(async function () {
+        await driver.quit();
+      });
+    });
+  
+  
+  });
